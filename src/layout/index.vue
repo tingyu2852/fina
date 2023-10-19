f3af43e20d2f1dfac10ec4671f84662c1f6b8d69
zhaohailong <1254557232@qq.com>
2023-10-19 21:01:50 +0800

    用户首页项目名增加和退出功能

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,8 +1,21 @@
 <template>
   <div :class="classObj" class="app-wrapper">
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+    
     <sidebar class="sidebar-container" />
+    
     <div class="main-container">
+      <div class="main-content-head">
+        <div class="project-name">
+          <span>经开建设项目管理平台</span>
+        </div>
+        <div class="user-info-wrap">
+            <div>用户名：{{ this.$store.state.user.name }}</div>
+            <div @click.native="logout" class="exit-login">
+              <span>退出登录</span>
+            </div>
+        </div>
+      </div>
       <div :class="{'fixed-header':fixedHeader}">
         <navbar />
       </div>
@@ -45,6 +58,11 @@ export default {
   methods: {
     handleClickOutside() {
       this.$store.dispatch('app/closeSideBar', { withoutAnimation: false })
+    },
+    // 退出功能
+    async logout() {
+      await this.$store.dispatch('user/logout')
+      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
     }
   }
 }
@@ -53,6 +71,37 @@ export default {
 <style lang="scss" scoped>
   @import "~@/styles/mixin.scss";
   @import "~@/styles/variables.scss";
+
+  .main-content-head{
+    display: flex;
+    height: 50px;
+    align-items: center;
+    width: 100%;
+    .project-name{
+      width: 60%;
+      align-items: center;
+      padding-left: 12px;
+      font-size: 24px;
+      letter-spacing: 1px;
+      color: #333;
+      font-weight: 600;
+    }
+    .user-info-wrap{
+       width: 40%;
+       justify-content: right;
+       display: flex;
+       padding-right: 20px;
+       align-items: center;
+       .exit-login{
+         padding-left: 6px;
+         cursor: pointer;
+       }
+      }
+      .exit-login:hover{
+         color: green;
+       
+      }
+  }  
 
   .app-wrapper {
     @include clearfix;
